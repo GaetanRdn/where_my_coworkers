79e0c46deb76f6f2c3b1ac01e23558853df8db57
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-11-16 04:50:25 +0100

    feature(EasymakerForm): Update form title
    Closes #1

--- lib/easymaker_form.dart
+++ lib/easymaker_form.dart
@@ -12,9 +12,7 @@ class _EasymakerFormPageState extends State<EasymakerFormPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Easymaker form'),
+        title: const Text('New Easymaker'),
       ),
       body: Center(
         child: Padding(
