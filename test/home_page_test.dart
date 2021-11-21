8012dcfe9c80b3bbc9d44ea5f29b3ea7574077cb
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-11-21 15:55:41 +0100

    test(HomePage): fix test
    Closes #4

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,4 +1,4 @@
-import 'package:easymakers_tracker/easymaker_form.dart';
+import 'package:easymakers_tracker/easymakers_page.dart';
 import 'package:easymakers_tracker/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -14,7 +14,7 @@ void main() {
     expect(find.widgetWithText(AppBar, 'Home'), findsOneWidget);
     expect(
         find.byWidgetPredicate((widget) => widget is PageView), findsOneWidget);
-    expect(find.byWidgetPredicate((widget) => widget is EasymakerFormPage),
+    expect(find.byWidgetPredicate((widget) => widget is EasymakersPage),
         findsOneWidget);
 
     expect(find.byWidgetPredicate((widget) => widget is BottomNavigationBar),
