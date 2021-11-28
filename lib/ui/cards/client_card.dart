658637b55363a70eaeb212ab9f3aba1951f67ed0
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2021-11-28 16:41:14 +0100

    feature(ClientsPage): use cards (#45)
    * feature(ClientsPage): use cards
    
    Closes #43
    
    * feature(ClientsPage): use cards
    
    Closes #43
    
    Co-authored-by: Gaetan Redin <gaetan.redin@cbp-group.com>

---
+++ lib/ui/cards/client_card.dart
@@ -0,0 +1,21 @@
+import 'package:easymakers_tracker/models/client.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ClientCard extends StatelessWidget {
+  const ClientCard({Key? key, required this.client}) : super(key: key);
+
+  final Client client;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: ListTile(
+        leading: const CircleAvatar(
+            child: Icon(Icons.business)),
+        title: Text(client.name),
+      ),
+    );
+  }
+}
--- lib/ui/pages/clients_page.dart
+++ lib/ui/pages/clients_page.dart
@@ -1,6 +1,7 @@
 import 'package:easymakers_tracker/models/client.dart';
-import 'package:easymakers_tracker/ui/forms/client_form.dart';
 import 'package:easymakers_tracker/stores/client_storage.dart';
+import 'package:easymakers_tracker/ui/cards/client_card.dart';
+import 'package:easymakers_tracker/ui/forms/client_form.dart';
 import 'package:flutter/material.dart';
 
 class ClientsPage extends StatefulWidget {
@@ -26,52 +27,39 @@ class _EasymakersPage extends State<ClientsPage> {
     });
   }
 
-  List<DataRow> getRows(List<Client> clients) {
-    return clients.map((e) {
-      return DataRow(
-        cells: <DataCell>[
-          DataCell(Text(e.name)),
-          DataCell(Text(e.latitude.toString())),
-          DataCell(Text(e.longitude.toString())),
-        ],
-      );
-    }).toList();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-              padding: const EdgeInsets.all(16),
-              child: FutureBuilder(
-                  future: _clients,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Client>> snapshot) {
-                    return DataTable(columns: const <DataColumn>[
-                      DataColumn(label: Text('Name')),
-                      DataColumn(label: Text('Latitude')),
-                      DataColumn(label: Text('Longitude'))
-                    ], rows: getRows(snapshot.data ?? []));
-                  })),
-          Padding(
-              padding: const EdgeInsets.all(16),
-              child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: FloatingActionButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ClientFormPage()),
-                        ).then((value) => loadClients());
-                      },
-                      child: const Icon(Icons.add)))),
-        ],
-      )
-    ]);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: FutureBuilder(
+            future: _clients,
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
+              return ListView(
+                children: (snapshot.data ?? <Client>[]).map((client) {
+                  return ClientCard(client: client);
+                }).toList(),
+              );
+            },
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.all(16),
+            child: Align(
+                alignment: Alignment.bottomRight,
+                child: FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ClientFormPage()),
+                      ).then((value) => loadClients());
+                    },
+                    child: const Icon(Icons.add)))),
+      ],
+    );
   }
 }
